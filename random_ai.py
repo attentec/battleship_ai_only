0ba2878cc5417b5d2ef9735a53d3724a7f757968
Thomas Henriksson <thohe973@student.liu.se>
2020-03-02 15:40:49 +0100

    random ai

--- random_ai.py
+++ random_ai.py
@@ -1,15 +1,19 @@
 import random
+import time
 
-# Class name must be Ai
+
 class Ai():
+  """ AI implementation class. Class name must be Ai """
+
   def __init__(self, width, height):
     self.width = width
     self.height = height
+
     self.moves = self.__get_all_possible_moves()
+    random.shuffle(self.moves)
 
   def get_move(self, last_result):
     """
-    Needed for the AI.
     Called when it's time to make the next move
 
     :param last_result: The result of the last move, will be None the first round.
@@ -19,12 +23,10 @@ class Ai():
 
     :return: The coordinates to fire at as a list [x, y], upper left is [0, 0]
     """
-
-    return self.moves.pop(0)
+    return self.moves.pop()
 
   def place_ship(self, ship_len, valid_pos_fn):
     """
-    Needed for the AI.
     Called once for each ship.
 
     :param ship_len: The length of the next ship to place
